c8e94666b404047db22d65628af20dd9917c3829
Erik <erik@web-code.nl>
2023-11-06 16:13:58 +0100

    env2

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
